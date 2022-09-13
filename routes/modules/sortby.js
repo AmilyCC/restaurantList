e17fd01494d2abc95e2d3cca84b38e6333cc14b6
N000101\Amily Chang <aymaiclhyu777@gmail.com>
2022-09-13 10:25:21 +0800

    renew badge class

--- routes/modules/sortby.js
+++ routes/modules/sortby.js
@@ -6,49 +6,43 @@ const Rest = require('../../models/rest') // 載入 Rest model
 //排序
 router.get('/', (req, res) => {
     const sort = req.query.sort
-    var sort1 = false
-    var sort2 = false
-    var sort3 = false
-    var sort4 = false
-    var sort5 = false
-    var sort6 = false
     if(sort === 'name'){
-        sort1 = true
+        var sort1 = sort
         Rest.find({})
             .lean()
             .sort({name: 'asc'})  
             .then(rests =>res.render('index', { rests, sort1 }))
             .catch(error => console.error(error)) // 錯誤處理
     }else if(sort === 'nameDesc'){
-        sort2 = true
+        var sort2 = sort
         Rest.find({})
             .lean()
             .sort({name: 'desc'})  
             .then(rests =>res.render('index', { rests, sort2 }))
             .catch(error => console.error(error)) // 錯誤處理
     }else if(sort === 'rateDesc'){
-        sort3 = true
+        var sort3 = sort
         Rest.find({})
             .lean()
             .sort({rating: 'desc'})  
             .then(rests =>res.render('index', { rests, sort3 }))
             .catch(error => console.error(error)) // 錯誤處理
     }else if(sort === 'rate'){
-        sort4 = true
+        var sort4 = sort
         Rest.find({})
             .lean()
             .sort({rating: 'asc'})  
             .then(rests =>res.render('index', { rests, sort4 }))
             .catch(error => console.error(error)) // 錯誤處理
     }else if(sort === 'cate'){
-        sort5 = true
+        var sort5 = sort
         Rest.find({})
             .lean()
             .sort({category: 'asc'})  
             .then(rests =>res.render('index', { rests, sort5 }))
             .catch(error => console.error(error)) // 錯誤處理
     }else {
-        sort6 = true
+        var sort6 = sort
         Rest.find({})
             .lean()
             .sort({location: 'asc'})  
